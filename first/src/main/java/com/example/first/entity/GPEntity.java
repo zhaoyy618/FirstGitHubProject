3aef213938c0bbc3bdeac78823d6ff1af3540db1
zhaoyy618 <1553103832@qq.com>
2022-05-22 20:42:46 +0800

    update:master修改addText文件,添加fruits

---
+++ first/src/main/java/com/example/first/entity/GPEntity.java
@@ -0,0 +1,12 @@
+package com.example.first.entity;
+
+import lombok.Data;
+
+@Data
+public class GPEntity {
+    private String day;
+    private String open;
+    private String high;
+
+    private String volume;
+}
